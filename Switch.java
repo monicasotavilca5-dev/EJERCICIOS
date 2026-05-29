cad816e61fed9ece9a11fa8042c838c9bc44bdca
monicasotavilca5-dev <monicasotavilca5@gmail.com>
2026-05-29 22:22:09 +0200

    Add files via upload
    ejercicio1 dia 1 a 7 usando switch

---
+++ Switch.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número del 1 al 7:");
+        int dia = scanner.nextInt();
+
+        switch (dia) {
+
+            case 1:
+                System.out.println("Lunes");
+                break;
+
+            case 2:
+                System.out.println("Martes");
+                break;
+
+            case 3:
+                System.out.println("Miércoles");
+                break;
+
+            case 4:
+                System.out.println("Jueves");
+                break;
+
+            case 5:
+                System.out.println("Viernes");
+                break;
+
+            case 6:
+                System.out.println("Sábado");
+                break;
+
+            case 7:
+                System.out.println("Domingo");
+                break;
+
+            default:
+                System.out.println("Número no válido");
+        }
+
+    }
+}
